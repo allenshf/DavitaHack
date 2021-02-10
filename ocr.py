6d8fed0cd7b5fa00a1bc7fb39613618df1e66625
Hunter Pischke <61955557+HunterPischke@users.noreply.github.com>
2021-02-10 14:58:14 -0600

    Text recognition ocr

--- ocr.py
+++ ocr.py
@@ -1,29 +1,6 @@
 import io
 import os
-
-# Imports the Google Cloud client library
 from google.cloud import vision
-
-# Instantiates a client
-client = vision.ImageAnnotatorClient()
-
-# The name of the image file to annotate
-file_name = os.path.abspath('wakeupcat.jpg')
-
-# Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read()
-
-image = vision.Image(content=content)
-
-# Performs label detection on the image file
-response = client.label_detection(image=image)
-labels = response.label_annotations
-
-print('Labels:')
-for label in labels:
-    print(label.description)
-
 
 
 def detect_document(path):
@@ -61,3 +38,7 @@ def detect_document(path):
             '{}\nFor more info on error messages, check: '
             'https://cloud.google.com/apis/design/errors'.format(
                 response.error.message))
+
+
+
+detect_document('wakeupcat.jpg')
