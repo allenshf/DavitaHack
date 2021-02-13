6a0bf40e14d1d223b617490e755c0ca754e49815
Allen Shufer <allenshufer@utexas.edu>
2021-02-12 18:41:11 -0600

    Hide Key

--- davita/davita/settings.py
+++ davita/davita/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nzzre&*(8b*980sf@^2qr*ls7mpjj5eoea44y*0_s!l1ifpz_p'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
